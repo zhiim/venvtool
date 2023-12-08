c30f07787656ea2578dfeb2766400c7a119f0f32
Q Xu <m3196253580@gmail.com>
2023-12-08 12:34:20 +0800

    add options parse; venv activate need to be rebuilt

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <string>
+#include "cxxopts.hpp"
 #include "venvtool.hpp"
 
-int main(int argn, ) {
+int main(int argc, char** argv) {
+    cxxopts::Options options("venvtool", "a tool to manage venvs like conda");
+
+    options.add_options()
+        ("c, create", "Create a new venv", cxxopts::value<std::string>())
+        ("l, list", "List all existing venvs")
+        ("a, activate", "Activate an venv", cxxopts::value<std::string>())
+        ("d, deactivate", "Deactivate an venv")
+        ("r, remove", "Remove an venv", cxxopts::value<std::string>())
+        ("h, help", "Print usage")
+    ;
+
+    auto result = options.parse(argc, argv);
+
+    std::string venvName;
+    VenvTool venvTool("/home/xu/.venvtool");
     
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        exit(0);
+    }
+    if (result.count("create")) {
+        venvName = result["create"].as<std::string>();
+        venvTool.venvCreate(venvName);
+    }
+    if (result.count("list")) {
+        venvTool.venvList();
+    }
+    if (result.count("activate")) {
+        venvName = result["activate"].as<std::string>();
+        venvTool.venvActivate(venvName);
+    }
+    if (result.count("deactivate")) {
+        venvTool.venvDeactive();
+    }
+    if (result.count("remove")) {
+        venvName = result["remove"].as<std::string>();
+        venvTool.venvRemove(venvName);
+    }
+
+    return 0;
 }
--- src/venvtool.cpp
+++ src/venvtool.cpp
@@ -7,9 +7,11 @@ VenvTool::VenvTool(std::string venvPath) {
     venvPath_ = venvPath;
 }
 
+VenvTool::~VenvTool() {}
+
 void VenvTool::venvCreate(std::string venvName) {
-    std::string createCMD = "python -m venv";
-    createCMD += (venvPath_ + venvName);
+    std::string createCMD = "python -m venv ";
+    createCMD += (venvPath_ + "/" + venvName);
     std::system(createCMD.c_str());
 }
 
@@ -22,19 +24,20 @@ void VenvTool::venvList() {
     for (auto const& entry : std::filesystem::directory_iterator(venvRootPath))
     {
         // print venv name and path
-        std::cout << std::setw(24) << std::right << entry.path().filename()\
+        std::cout << std::setw(24) << std::left << entry.path().filename()\
                     << entry.path() << std::endl;
     }
 }
 
 void VenvTool::venvActivate(std::string venvName) {
-    #ifdef _WIN32 || __MINGW32__
-    std::string activateCMD = venvPath_ + venvName + "\\Scripts\\activate";
+    #if defined(_WIN32) || defined(__MINGW32__)
+    std::string activateCMD = venvPath_ + "\\" + venvName + "\\Scripts\\activate";
     #else
-    std::string activateCMD = "source " + venvPath_ + venvName +\
+    std::string activateCMD = "source " + venvPath_ + "/" + venvName +\
                             "/bin/activate";
     #endif
 
+    std::cout << activateCMD << std::endl;
     std::system(activateCMD.c_str());
 }
 
@@ -43,6 +46,6 @@ void VenvTool::venvDeactive() {
 }
 
 void VenvTool::venvRemove(std::string venvName) {
-    std::filesystem::path venvPathToBeRemoved(venvPath_ + venvName);
+    std::filesystem::path venvPathToBeRemoved(venvPath_ + "/" + venvName);
     std::filesystem::remove_all(venvPathToBeRemoved);
 }
